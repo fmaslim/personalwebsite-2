a1d027ec2dbe7ec76127620d58ef1b7638c19cdf
fmaslim <franky.maslim@yahoo.com>
2026-04-15 12:06:35 -0400

    Added DTO validations

--- PersonalWebsite.Api/DTOs/CreateOrderDto.cs
+++ PersonalWebsite.Api/DTOs/CreateOrderDto.cs
@@ -1,12 +1,19 @@
-﻿namespace PersonalWebsite.Api.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebsite.Api.DTOs
 {
     public class CreateOrderDto
     {
+        [Range(1, int.MaxValue)]
         public int CustomerId { get; set; }
         public DateTime OrderDate { get; set; }
+        [Range(0.01, double.MaxValue)]
         public decimal TotalAmount { get; set; }
         public int BillToAddressId { get; set; }
         public int ShipToAddressId { get; set; }
         public int ShipMethodId { get; set; }
+        [Required]
+        [StringLength(1)]
+        public string Notes { get; set; } = string.Empty;
     }
 }
