ad38e7ce2bb21e8bd0bd1ee3eb5059b9162a00d2
fmaslim <franky.maslim@yahoo.com>
2026-04-21 04:47:30 -0400

    Added a fake token to v2 response

--- PersonalWebsite.Api/Controllers/AuthV2Controller.cs
+++ PersonalWebsite.Api/Controllers/AuthV2Controller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 using PersonalWebsite.Api.DTOs;
 
 namespace PersonalWebsite.Api.Controllers
@@ -8,17 +9,33 @@ namespace PersonalWebsite.Api.Controllers
     public class AuthV2Controller : ControllerBase
     {
         [HttpPost("login")]
-        public IActionResult Login(LoginRequestDto dto)
+        public ActionResult<LoginResponseV2Dto> Login(LoginRequestDto dto)
         {
             var response = new LoginResponseV2Dto
             {
                 Username = dto.Username,
-                Message = "Login successful",
+                Message = "Login successful from v2",
                 Version = "v2",
-                ExpiresIn = 3600
+                ExpiresIn = 3600,
+                Token = "fake-jwt-token-v2",
+                RefreshToken = "fake-refresh-token-v2"
             };
             // Implement your login logic here
             return Ok(response);
         }
+
+        //[HttpPost("login")]
+        //public IActionResult Login(LoginRequestDto dto)
+        //{
+        //    var response = new LoginResponseV2Dto
+        //    {
+        //        Username = dto.Username,
+        //        Message = "Login successful",
+        //        Version = "v2",
+        //        ExpiresIn = 3600
+        //    };
+        //    // Implement your login logic here
+        //    return Ok(response);
+        //}
     }
 }
--- PersonalWebsite.Api/DTOs/LoginResponseV2Dto.cs
+++ PersonalWebsite.Api/DTOs/LoginResponseV2Dto.cs
@@ -6,5 +6,8 @@
         public string Message { get; set; } = string.Empty;
         public string Version { get; set; } = string.Empty;
         public int ExpiresIn { get; set; }
+
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
     }
 }
