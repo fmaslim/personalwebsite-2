af008b4f461aa34a9758bc1573556579f81e8041
fmaslim <franky.maslim@yahoo.com>
2026-04-26 16:08:30 -0400

    Added validate bad paging input

--- PersonalWebsite.Api/DTOs/Common/PagingRequestDto.cs
+++ PersonalWebsite.Api/DTOs/Common/PagingRequestDto.cs
@@ -4,14 +4,19 @@
     {
         private const int MaxPageSize = 100;
 
-        public int PageNumber { get; set; } = 1;
-
+        private int _pageNumber = 1;
         private int _pageSize = 10;
+
+        public int PageNumber 
+        {
+            get => _pageNumber;
+            set => _pageNumber = value < 1 ? 1 : value;
+        }
 
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+            set => _pageSize = value < 1 ? 10 : value > MaxPageSize ? MaxPageSize : value;
         }
     }
 }
