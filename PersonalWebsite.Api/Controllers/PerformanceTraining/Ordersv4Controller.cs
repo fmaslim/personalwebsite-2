1c028f4ba8b6d449278e235a934487590dc28380
fmaslim <franky.maslim@yahoo.com>
2026-05-04 13:30:46 -0400

    Get ILogger / full exceptions into App Insights

--- PersonalWebsite.Api/Controllers/PerformanceTraining/Ordersv4Controller.cs
+++ PersonalWebsite.Api/Controllers/PerformanceTraining/Ordersv4Controller.cs
@@ -39,5 +39,30 @@ namespace PersonalWebsite.Api.Controllers.PerformanceTraining
         {
             throw new NotImplementedException();
         }
+
+        [HttpGet("app-insight-200")]
+        [Produces("application/json")]
+        public IActionResult Endpoint_200()
+        {
+            return Ok("AppInsight test 200 Ok");
+        }
+
+        [HttpGet("app-insight-400")]
+        public IActionResult Endpoint_400()
+        {
+            return BadRequest("AppInsight test 400 Bad Request");
+        }
+
+        [HttpGet("app-insight-404")]
+        public IActionResult Endpoint_404()
+        {
+            return NotFound("AppInsight test 404 Not Found");
+        }
+
+        [HttpGet("app-insight-409")]
+        public IActionResult Endpoint_409()
+        {
+            return Conflict("AppInsight test 409 Conflict");
+        }
     }
 }
